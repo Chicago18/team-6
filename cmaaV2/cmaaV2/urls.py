e2c68899d6029fc79cc927ca735b8190362bee7d
Hector Santillan Vazquez <Hectorsv@Hectors-MacBook-Pro.local>
2018-10-13 07:21:27 -0500

    consent form screens

--- cmaaV2/cmaaV2/urls.py
+++ cmaaV2/cmaaV2/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+from django.views.generic.base import TemplateView
+from django.contrib.auth.views.LoginView
+from django.contrib.auth.views.LogoutView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    	path('admin/', admin.site.urls),
+    	url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    	url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    	url(r'^logout/$', auth_views.logout, {'template_name': 'logged_out.html'}, name='logout'),
 ]
