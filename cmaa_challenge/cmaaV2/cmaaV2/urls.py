20115165ac5767a1698816b7de3560507447f62a
MikeO <ozman4@gmail.com>
2018-10-13 00:50:35 -0500

    Adding the new version

--- cmaa_challenge/cmaaV2/cmaaV2/urls.py
+++ cmaa_challenge/cmaaV2/cmaaV2/urls.py
@@ -1,4 +1,4 @@
-"""cmaa_project URL Configuration
+"""cmaaV2 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -14,10 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
-from django.views.generic import TemplateView
+from django.urls import path
+
 urlpatterns = [
-  path('admin/', admin.site.urls),
-  # path('api/', include('mynewapp.urls')),
-  re_path('.*', TemplateView.as_view(template_name='index.html')),
+    path('admin/', admin.site.urls),
 ]
