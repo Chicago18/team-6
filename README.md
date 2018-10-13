208aa2d7340a986687a76afc8836b4998ac9ea2e
MikeO <ozman4@gmail.com>
2018-10-13 09:06:45 -0500

    Changing Readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # team-6
+
+## Challenge:  Our challenge was to work with the CMAA non-profit and streamline their learning process for students. We automate the form creation as well as tailor the learning. 
--- cmaa_rails/cmaa/db/schema.rb
+++ cmaa_rails/cmaa/db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2018_10_13_073818) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "forms", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
